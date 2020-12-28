0716d043819e736d313382981a530810b772c272
niyigenayves <wizynives@gmail.com>
2020-12-28 10:38:44 +0200

    nyigisha files

--- Registration-page/validation.php
+++ Registration-page/validation.php
@@ -5,7 +5,7 @@ mysqli_select_db($con, 'Nyigisha_db');
 $name = $_POST['school'];
 $pass = $_POST['password'];
 $pass1 =md5($pass);
-$s = " select * from registration where school_name = '$name'&& password = '$pass1'";
+$s = " select * from registration where school_name = '$name' && password = '$pass1'";
 $result = mysqli_query($con, $s);
 $num = mysqli_num_rows($result);
 if ($num ==1){
